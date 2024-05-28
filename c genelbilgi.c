aad61583e6236d1629f31510c3d8183aed99a408
zorbeyyavas <yavaszorbey@gmail.com>
2024-05-29 01:48:47 +0300

    Add files via upload

---
+++ c genelbilgi.c
@@ -0,0 +1,46 @@
+
+int main(){
+// açıklama satırı yapar
+/*iki * arasını açıklama satırı haline getirir */
+/* \n yeni satıra geçer */
+/* \t geniş bir boşluk bırakır */
+/* */
+
+
+//printf(" ahmet \n yasin \n teyze \n ");
+
+//printf("\"ahmetin annesi olmus\" birakin gelsin");
+
+/*int level = 87 ;//tam sayı
+float xp = 12.05;//ondalklı sayı
+char server ='A';//tek karakter
+char nick[]="aslanim";//karakter dizesi
+
+printf("merhaba %s hosgeldin",nick); printf("\n");
+printf("suan %d levele sahpsin\n",level);
+printf("xp durumun %%%f\n",xp);
+printf("suanda %c sunucusunda oynuyorsun",server); */
+
+//----------------------------------------------------------------------------
+
+//kullanıcıdan veri alma
+//scanf ilk yazdığımız veriden sonra boşluk koyarsak 2. veri algılıyoor
+//boşluk koymamz gereken girişlerde fgets() kullanıyor kulanımı farklıdır aşağda örnekte var
+/*int yas;
+
+char isim[25];
+
+printf("adiniz nedir\n");
+fgets(isim,25,stdin);
+
+printf("kac yasindasin\n");
+scanf("%d",&yas);
+//scanf("%s",&isim);
+
+printf("memnun oldum %s",isim);
+printf("%d yasindasin\n",yas); */
+
+
+
+
+}
